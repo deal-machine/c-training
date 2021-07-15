a68ed5f36e2275afbf667da32236f1f5f8d294b9
deal <95deal@gmail.com>
2021-07-14 23:25:34 -0300

    doubry linked list getting values

--- src/estrutura/dinamicos/dinamic.c
+++ src/estrutura/dinamicos/dinamic.c
@@ -16,43 +16,69 @@ typedef struct lista
 
 Lista *cria_lista();
 void libera_lista(Lista *li);
-void insere_inicio(Lista *li, int valor);
-void insere_ordenado(Lista *li, int valor);
-void insere_fim(Lista *li, int valor);
-void imprime_lista(Lista *li);
+
+int insere_inicio(Lista *li, int valor);
+int insere_ordenado(Lista *li, int valor);
+int insere_fim(Lista *li, int valor);
+
 int remove_inicio(Lista *li);
 int remove_fim(Lista *li);
+int remove_valor(Lista *li, int valor);
+
+void visualiza_valor(Lista *li, int valor);
+void visualiza_posicao(Lista *li, int posicao);
+void imprime_lista(Lista *li);
 
 int main()
 {
   Lista *li = cria_lista();
-  int escolha = 0, valor, remove = 0;
+  int escolha = 0, valor, remove = 0, insere = 0, visualiza = 0, posicao;
 
   do
   {
-    printf("\n******************************\nInforme uma opção\n1-Inserir no começo\n2-Inserir ordenado\n3-Inserir no fim\n4-Remover valor\n5-Imprimir Lista\nQualquer outra tecla para sair\n******************************\nEscolha: ");
+    printf("\n******************************\nTrabalhando com LISTAS\nInforme uma opção\n\n1-Inserir\n2-Remover valor\n3-Visualizar\nQualquer outra tecla para sair\n******************************\nEscolha: ");
     scanf("%d", &escolha);
 
     switch (escolha)
     {
     case 1:
-      printf("\nInforme um valor para inserir no começo: ");
-      scanf("%d", &valor);
-      insere_inicio(li, valor);
+      printf("\n1-Inserir no começo\n2-Inserir ordenado\n3-Inserir no fim\n4-Voltar\nEscolha: ");
+      scanf("%d", &insere);
+      switch (insere)
+      {
+      case 1:
+        printf("\nInforme um valor para inserir no começo: ");
+        scanf("%d", &valor);
+        if (insere_inicio(li, valor))
+          printf("\nValor inserido com sucesso.\n");
+        else
+          printf("\nFalha ao inserir valor.\n");
+        break;
+      case 2:
+        printf("\nInforme um valor para inserir ordenado: ");
+        scanf("%d", &valor);
+        if (insere_ordenado(li, valor))
+          printf("\nValor inserido com sucesso.\n");
+        else
+          printf("\nFalha ao inserir valor.\n");
+        break;
+      case 3:
+        printf("\nInforme um valor para inserir no fim: ");
+        scanf("%d", &valor);
+        if (insere_fim(li, valor))
+          printf("\nValor inserido com sucesso.\n");
+        else
+          printf("\nFalha ao inserir valor.\n");
+        break;
+      case 4:
+        printf("\nVoltando...\n");
+        break;
+      default:
+        break;
+      }
       break;
     case 2:
-      printf("\nInforme um valor para inserir ordenado: ");
-      scanf("%d", &valor);
-      insere_ordenado(li, valor);
-      break;
-    case 3:
-      printf("\nInforme um valor para inserir no fim: ");
-      scanf("%d", &valor);
-      insere_fim(li, valor);
-      break;
-    case 4:
-
-      printf("\n1-Remover do inicio\n2-Remover por referência\n3-Remover do fim.\n4-Voltar\nEscolha: ");
+      printf("\n1-Remover do inicio\n2-Remover valor\n3-Remover do fim.\n4-Voltar\nEscolha: ");
       scanf("%d", &remove);
       switch (remove)
       {
@@ -63,7 +89,12 @@ int main()
           printf("\nErro ao remover do inicio.\n");
         break;
       case 2:
-        printf("remover por referencia");
+        printf("\nInforme o valor para remover: ");
+        scanf("%d", &valor);
+        if (remove_valor(li, valor))
+          printf("\nValor removido.\n");
+        else
+          printf("\nErro ao remover valor.\n");
         break;
       case 3:
         if (remove_fim(li))
@@ -77,17 +108,38 @@ int main()
       default:
         break;
       }
-
       break;
-    case 5:
-      printf("\nLista: ");
-      imprime_lista(li);
+    case 3:
+      printf("\n1-Visualizar por posição\n2-Visualizar por valor\n3-Visualizar todas as informações.\n4-Voltar\nEscolha: ");
+      scanf("%d", &visualiza);
+      switch (visualiza)
+      {
+      case 1:
+        printf("\nInforme a posicao: ");
+        scanf("%d", &posicao);
+        visualiza_posicao(li, posicao);
+        break;
+      case 2:
+        printf("\nInforme o valor: ");
+        scanf("%d", &valor);
+        visualiza_valor(li, valor);
+        break;
+      case 3:
+        printf("\nLista: ");
+        imprime_lista(li);
+        break;
+      case 4:
+        printf("\nVoltando...");
+        break;
+      default:
+        break;
+      }
       break;
     default:
       escolha = 0;
       printf("\nSaindo...\n");
     }
-  } while (escolha >= 1 && escolha <= 5);
+  } while (escolha >= 1 && escolha <= 3);
 
   libera_lista(li);
   system("pause");
@@ -109,8 +161,10 @@ void libera_lista(Lista *li)
   free(li);
 }
 
-void insere_inicio(Lista *li, int valor)
+int insere_inicio(Lista *li, int valor)
 {
+  if (li == NULL)
+    return 0;
 
   No *novo = (No *)malloc(sizeof(No));
 
@@ -123,10 +177,14 @@ void insere_inicio(Lista *li, int valor)
 
   li->inicio = novo;
   li->tamanho++;
+
+  return 1;
 }
 
-void insere_fim(Lista *li, int valor)
+int insere_fim(Lista *li, int valor)
 {
+  if (li == NULL)
+    return 0;
 
   No *novo = (No *)malloc(sizeof(No));
 
@@ -150,10 +208,15 @@ void insere_fim(Lista *li, int valor)
   }
 
   li->tamanho++;
+
+  return 1;
 }
 
-void insere_ordenado(Lista *li, int valor)
+int insere_ordenado(Lista *li, int valor)
 {
+  if (li == NULL)
+    return 0;
+
   No *novo = (No *)malloc(sizeof(No));
 
   novo->valor = valor;
@@ -189,6 +252,8 @@ void insere_ordenado(Lista *li, int valor)
     }
   }
   li->tamanho++;
+
+  return 1;
 }
 
 void imprime_lista(Lista *li)
@@ -243,4 +308,61 @@ int remove_fim(Lista *li)
   li->tamanho--;
 
   return 1;
+}
+
+int remove_valor(Lista *li, int valor)
+{
+  if (li == NULL || li->inicio == NULL)
+    return 0;
+
+  No *no = li->inicio;
+
+  while (no->proximo != NULL && no->valor != valor)
+    no = no->proximo;
+
+  if (no->anterior == NULL)
+    li->inicio = no->proximo;
+  else
+    no->anterior->proximo = no->proximo;
+
+  if (no->proximo != NULL)
+    no->proximo->anterior = no->anterior;
+
+  free(no);
+
+  li->tamanho--;
+}
+
+void visualiza_valor(Lista *li, int valor)
+{
+  if (li == NULL || li->inicio == NULL)
+    printf("\nLista inexistente.\n");
+  else
+  {
+
+    No *no = li->inicio;
+
+    while (no->proximo != NULL && no->valor != valor)
+      no = no->proximo;
+
+    printf("\nValor: %d\n", no->valor);
+  }
+}
+
+void visualiza_posicao(Lista *li, int posicao)
+{
+  if (li == NULL || li->inicio == NULL || posicao <= 0)
+    printf("\nLista inexistente.\n");
+  else
+  {
+
+    No *no = li->inicio;
+    int i = 0;
+    while (no->proximo != NULL && i < posicao)
+    {
+      no = no->proximo;
+      i++;
+    }
+    printf("\nValor: %d\n", no->valor);
+  }
 }
